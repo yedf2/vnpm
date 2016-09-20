49e70e50a51e09ccd9269e76d45aa6f5ff5c774e
yedf <dongfuye@163.com>
2016-09-20 09:37:03 +0800

    updated

--- vnpm.js
+++ vnpm.js
@@ -12,6 +12,6 @@ p.version = `${vers[0]}.${vers[1]}.${v3}`;
 let news = JSON.stringify(p, null, 4);
 fs.writeFileSync(f, news);
 console.log(`old version: ${oldv}, new version: ${p.version}`);
-let cmd = `npm publish ${path} && cnpm sync ${p.name}`;
+let cmd = `npm publish ${path} --registry=https://registry.npmjs.org && cnpm sync ${p.name}`;
 console.log(cmd);
 execSync(cmd, {stdio:[0,1,2]})
